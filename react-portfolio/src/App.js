0d9576018978f8b30050187fcf73abe3ce92976b
Buubonic <nguyenb.jim@gmail.com>
2023-05-20 16:26:00 -0700

    add react version of portfolio

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import './App.css';
+
+function App() {
+
+  const [pages] = useState ([
+    { name: "About" },
+    { name: "Project" },
+    { name: "Resume" }
+  ]);
+
+  const [ currentPage, setCurrentPage ] = useState( pages[0] );
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
