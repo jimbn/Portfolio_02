16657b45b82bc8b718af772fb76917f43b69299f
Buubonic <nguyenb.jim@gmail.com>
2023-05-24 21:15:17 -0700

    shift nav button

--- react-portfolio/src/components/Header/index.js
+++ react-portfolio/src/components/Header/index.js
@@ -5,17 +5,13 @@ import Nav from "../Nav";
 function Header ({pages, setCurrentPage}) {
 
   return(
-    <header style={{ display: 'flex', justifyContent:'space-between' }}>
+    <header style={{ display: 'flex'}}>
       <h1> Jim Bui Nguyen</h1>
-      <div>
+      <div style={{ paddingLeft:"1rem"}}>
         <NavHam 
           pages={pages}
           setCurrentPage={setCurrentPage}
         />
-        {/* <Nav 
-          pages={pages}
-          setCurrentPage={setCurrentPage}
-        /> */}
       </div>
     </header>
   )  
